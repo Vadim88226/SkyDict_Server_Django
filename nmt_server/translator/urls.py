070acce3439055d3ad7413f5fe73ca1c4e9b3f78
figaro <figaro@users.noreply.github.com>
2020-02-14 20:57:22 +1000

    FIGARO: Add omitted files

---
+++ nmt_server/translator/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from . import views
+app_name = 'translator'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('query_dict/', views.query_dict, name='query_dict'),
+    path('trans_sentences/', views.trans_sentences, name='trans_sentences'),
+]
