a217b8f748621f779ec03d6164b86c705231429b
figaro <figaro@users.noreply.github.com>
2020-02-22 01:45:38 +1000

    FIGARO: Resolve the conflict files

--- nmt_server/translator/static/dictionary/dictionary.js
+++ nmt_server/translator/static/dictionary/dictionary.js
@@ -41,12 +41,15 @@ function similar_words() {
 $(function(){
     $('#id_find_word').on('keyup', function(e) {
         var keycode = (e.keyCode ? e.keyCode : e.which);
-        if(keycode==13) {
+        if(keycode==13 || keycode==32) {
             $(".btn_search").click(); return;
         }
         clearTimeout(wait1);
         wait1 = setTimeout(similar_words, 200);
-    });    
+    });
+    $('#id_find_word').on('change', function(e) {
+        ShowSelection($('#id_find_word').val());
+    });
     $(document).on('click',"#wordDict_help_popup ul", function(e){
         var _content = e.currentTarget.children[0];
         $('#id_find_word').val(_content.textContent);
--- nmt_server/translator/views.py
+++ nmt_server/translator/views.py
@@ -1,5 +1,5 @@
 import re
-
+import linecache
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
 from django.template import loader
